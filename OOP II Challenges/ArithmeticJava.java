5cc21e8318b62b5105dfa0de6c72c5e1f5f9827e
natalie-abwoga <natalie.abwoga@strathmore.edu>
2025-07-01 07:19:12 +0300

    Illustrates the divide by 0 exception

---
+++ OOP II Challenges/ArithmeticJava.java
@@ -0,0 +1,10 @@
+import java.io.*;
+public class ArithmeticJava {
+    public static void main(String[] args) {
+        int number1 = 100;
+        int number2 = 0;
+        int answer = number1/number2;
+        System.out.println("Answer: " +answer);
+    }
+
+}
