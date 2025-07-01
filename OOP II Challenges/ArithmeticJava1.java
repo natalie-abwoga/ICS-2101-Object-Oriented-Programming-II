1e507c07511454ca2dbc6afa520635e30dd0b6e3
natalie-abwoga <natalie.abwoga@strathmore.edu>
2025-07-01 07:22:30 +0300

    Handles the divide by 0 exception

---
+++ OOP II Challenges/ArithmeticJava1.java
@@ -0,0 +1,20 @@
+package Arithmetic;
+
+public class ArithmeticJava1 {
+    public static void main(String[] args) {
+        int number1 = 300;
+        int number2 = 0;
+
+        try{
+            int answer = number1/number2;
+            System.out.println("Answer: " + answer);
+        }
+        catch (ArithmeticException e) {
+            System.out.println(
+                    "Error: Division by zero is not allowed!");
+        }
+        finally {
+            System.out.println("Program continues after handling the exception.");
+        }
+    }
+}
